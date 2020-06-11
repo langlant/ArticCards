c9f3d94b6a8c5f5e138a034c1e812b10517311f8
langlant <65258571+langlant@users.noreply.github.com>
2020-06-10 23:57:52 -0400

    Update App.js

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import CardScreen from './screens/CardScreen';
 import ProgressScreen from './screens/ProgressScreen';
 
 const Stack = createStackNavigator();
+
 export default function App() {
   return (
     <NavigationContainer>
