ff45dc7a15ea12ea37b92dab7ae53c6430a3c292
langlant <65258571+langlant@users.noreply.github.com>
2020-06-16 13:56:11 -0400

    yup

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -24,7 +24,7 @@ const MapScreen = ({navigation, route}) =>{
     return(
         <div style={{width: '100vw', height: '100vh'}}>
             <WrappedMap 
-                googleMapURL={`https://maps.googleapis.com/maps/api/place/textsearch/json?query=speech+pathologists&key=${gkey}`}
+                googleMapURL={getMap()}
                 loadingElement={<div style={{ height: `100%`}}/> }
                 containerElement={<div style={{ height: `100%` }} />}
                 mapElement={<div style={{ height: `100%` }} />}
