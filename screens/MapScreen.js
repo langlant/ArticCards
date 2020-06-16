228c4fa8c2cd510972d89653983e77e096cb80d6
langlant <65258571+langlant@users.noreply.github.com>
2020-06-16 13:07:47 -0400

    maps rework... still broken

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -8,6 +8,14 @@ import {
   } from "react-google-maps";
 import {getMap} from '../api/gmap';
 
+function Map(){
+    return(
+        <GoogleMap 
+            defaultZoom={10}
+            defaultCenter={{lat: 25, lng: 25}}
+        />
+    )
+}
 
 const WrappedMap = withScriptjs(withGoogleMap(Map));
 
@@ -16,12 +24,12 @@ const MapScreen = ({navigation, route}) =>{
     return(
         <div style={{width: '100vw', height: '100vh'}}>
             <WrappedMap 
-        googleMapURL={getMap()}
+                googleMapURL={getMap()}
                 loadingElement={<div style={{ height: `100%`}}/> }
                 containerElement={<div style={{ height: `100%` }} />}
                 mapElement={<div style={{ height: `100%` }} />}
             />
-        </div>
+            </div>
     )
 }
 
