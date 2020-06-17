1a5b86d53eb756ee74211e510ca008d668db1197
langlant <65258571+langlant@users.noreply.github.com>
2020-06-17 14:23:07 -0400

    google map (map undefined)

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -7,30 +7,66 @@ import {
     InfoWindow
   } from "react-google-maps";
 import {getMap} from '../api/gmap';
+import {gkey} from '../api/gkey'
 
-var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8
-  });
+const mapData = `http://maps.googleapis.com/maps/api/place/textsearch/json?query=speech+pathologists&key=${gkey}`
+
+function Map(){
+  const [selectedSpeech, setSelectedSpeech] = useState(null);
+
+  return(
+    <GoogleMap
+      defaultZoom={10} defaultCenter={{lat: 25, lng: 25}}
+    >
+      {mapData.results.map((speech) => (
+        <Marker key={speech.place_id} position={{
+          lat: speech.geometry.location.lat, 
+          lng: speech.geometry.location.lng
+        }}
+        onPress={() => {
+          setSelectedSpeech(speech);
+        }}
+        />
+      ))}
+
+      {selectedSpeech && (
+        <InfoWindow position={{
+          lat: selectedSpeech.geometry.location.lat, 
+          lng: selectedSpeech.geometry.location.lng
+        }}
+        onCloseClick={() => {
+          setSelectedSpeech(null);
+        }}
+        >
+          <div> 
+            <h2>
+              {selectedSpeech.name}
+            </h2>
+            <h3>
+              {selectedSpeech.rating}
+            </h3>
+            <p>
+              {selectedSpeech.formatted_address}
+            </p>
+          </div> 
+        </InfoWindow>
+      )}
+    </GoogleMap>
+  );
 }
 
-//const WrappedMap = withScriptjs(withGoogleMap(Map));
+const WrappedMap = withScriptjs(withGoogleMap(Map));
 
-const MapScreen = ({navigation, route}) =>{
-/*
-    return(
-        <div style={{width: '100vw', height: '100vh'}}>
-            <WrappedMap 
-                googleMapURL={getMap()}
-                loadingElement={<div style={{ height: `100%`}}/> }
-                containerElement={<div style={{ height: `100%` }} />}
-                mapElement={<div style={{ height: `100%` }} />}
-            />
-        </div>
-    )
-    */
+const MapScreen = () => {
+  return(
+    <div style={{width: '100vw', height: '100vh'}}>
+      <WrappedMap googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${gkey}`}
+        loadingElement = {<div style={{height: "100%"}}/>}
+        containerElement = {<div style={{height: "100%"}}/>}
+        mapElement = {<div style={{height: "100%"}}/>}
+      />
+    </div>
+  )
 }
 
 export default MapScreen;
